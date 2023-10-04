ef65d922ce87b7eec8ef92d9a402fea206041dcb
Bowofade Oyerinde <bowofadeoyerinde@gmail.com>
2023-10-04 12:44:38 +0100

    layouts created

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,30 +1,27 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700,800,900&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
+    <!-- Styles -->
+    @vite(['resources/css/app.css'])
+</head>
+<body class="relative flex-col items-center justify-center w-screen h-screen p-4 antialiased lg:flex bg-slate-200">
+    @yield('content')
+    <p class="px-6 py-1 my-2 font-medium bg-white border border-gray-300 rounded shadow-sm">
+        Designed and Coded by <a href="https://bowofade.com" class="inline-block italic font-medium text-center text-blue-500"><b>Bowofade</b></a>
+        <a target="_blank" class=" hover:text-green-600" href="https://github.com/oyenet1/ajaxtest">.View on github
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="inline bi bi-github" viewBox="0 0 16 16">
+                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+            </svg></a>
+    </p>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
-        </div>
-    </body>
+</body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,47 +1,32 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.guest')
+@section('content')
 
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700,800,900&display=swap" rel="stylesheet" />
-
-    <!-- Styles -->
-    @vite(['resources/css/app.css'])
-</head>
-<body class="relative flex-col items-center justify-center w-screen h-screen p-4 antialiased lg:flex bg-slate-200">
-    <div class="w-full max-w-xl p-4 py-8 mx-auto space-y-4 bg-white rounded-md shadow lg:py-12 xl:max-w-2xl">
-        <h1 class="text-2xl font-bold text-center capitalize">Technical Assesssment from automatepro</h1>
-        <div class="max-w-md p-3 mx-auto text-lg">
-            <p class="pb-2 text-xl font-medium">Questions</p>
-            <ol class="space-y-3 list-decimal list-inside">
-                <li>
-                    <span class="font-semibold">Show the Image with out page reload</span>
-                    <ul class="ml-4 list-disc list-inside">
-                        <li> Form</li>
-                        <li>Save Form data in Ajax</li>
-                        <li>Grid list in datatable in Ajax</li>
-                    </ul>
-                </li>
-                <li>
-                    <span class="font-semibold"> Please write the coding File Upload using AJax</span>
-                    <ul class="ml-4 list-disc list-inside">
-                        <li>Form</li>
-                        <li>Save Upload data in Ajax</li>
-                        <li>show the Image with out page reload</li>
-                    </ul>
-                </li>
-            </ol>
-
-        </div>
+<div class="w-full max-w-xl p-4 py-8 mx-auto space-y-4 bg-white rounded-md shadow lg:py-12 xl:max-w-2xl">
+    <h1 class="text-2xl font-bold text-center capitalize">Technical Assesssment from automatepro</h1>
+    <div class="max-w-md p-3 mx-auto text-lg">
+        <p class="pb-2 text-xl font-medium">Questions</p>
+        <ol class="space-y-3 list-decimal list-inside">
+            <li>
+                <span class="font-semibold">Show the Image with out page reload</span>
+                <ul class="ml-4 list-disc list-inside">
+                    <li> Form</li>
+                    <li>Save Form data in Ajax</li>
+                    <li>Grid list in datatable in Ajax</li>
+                </ul>
+            </li>
+            <li>
+                <span class="font-semibold"> Please write the coding File Upload using AJax</span>
+                <ul class="ml-4 list-disc list-inside">
+                    <li>Form</li>
+                    <li>Save Upload data in Ajax</li>
+                    <li>show the Image with out page reload</li>
+                </ul>
+            </li>
+        </ol>
     </div>
-    <p class="px-6 py-1 my-2 bg-white border border-gray-300 rounded shadow-sm">
-        Designed and Coded by <a href="https://bowofade.com" class="inline-block italic font-medium text-center text-blue-500"><b>Bowofade</b></a>
+    <p class="flex items-center justify-center space-x-4">
+        <a href="" class="inline-block px-6 py-2 text-white bg-blue-600 rounded-md shadow-sm b">Solution 1</a>
+        <a href="" class="inline-block px-6 py-2 text-white bg-blue-600 rounded-md shadow-sm b">Solution 2</a>
     </p>
-
-</body>
-</html>
+</div>
+@endsection
