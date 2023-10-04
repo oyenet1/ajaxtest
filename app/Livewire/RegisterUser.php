e92e62ceaebfd95ed593d4276f955f8d6a7f2290
Bowofade Oyerinde <bowofadeoyerinde@gmail.com>
2023-10-04 07:21:14 +0100

    first livewire created

---
+++ app/Livewire/RegisterUser.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class RegisterUser extends Component
+{
+    public function render()
+    {
+        return view('livewire.register-user');
+    }
+}
